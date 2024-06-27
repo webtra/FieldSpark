e62d0e03bd84d143f68a22a71d5de619da1a85be
Zascia Hugo <hugozascia@gmail.com>
2024-06-27 19:50:12 +0200

    Update Login.vue

--- resources/js/Pages/Auth/Login.vue
+++ resources/js/Pages/Auth/Login.vue
@@ -44,14 +44,14 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
-                <TextInput id="email" v-model="form.email" type="email" class="mt-1 block w-full" required autofocus />
+                <InputLabel for="email" value="Email Address" />
+                <TextInput id="email" v-model="form.email" type="email" placeholder="Email Address" class="mt-1 block w-full" required />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="mt-4">
                 <InputLabel for="password" value="Password" />
-                <TextInput id="password" v-model="form.password" type="password" class="mt-1 block w-full" required />
+                <TextInput id="password" v-model="form.password" type="password" placeholder="Password" class="mt-1 block w-full" required />
                 <InputError class="mt-2" :message="form.errors.password" />
             </div>
 

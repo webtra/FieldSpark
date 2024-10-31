dea736d7d0a8a6859152aaf7a712b79e30f834df
Zascia Hugo <hugozascia@gmail.com>
2024-10-31 19:16:48 +0200

    Update Dashboard.vue

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -1,8 +1,8 @@
 <template>
     <AppLayout title="Dashboard">
         <div>
-            <div class="flex justify-between">
-                <h2>Welcome, to your Dashboard!</h2>
+            <div class="justify-between">
+                                <p class="text-red-600 font-medium">This application is still in development.</p>
             </div>
         </div>
     </AppLayout>

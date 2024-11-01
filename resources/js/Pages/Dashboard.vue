c7ab5884ca10fe86d525e360431b9c04ce1c74e9
Zascia Hugo <hugozascia@gmail.com>
2024-11-01 12:46:28 +0200

    Updates

--- resources/js/Pages/Dashboard.vue
+++ resources/js/Pages/Dashboard.vue
@@ -2,7 +2,7 @@
     <AppLayout title="Dashboard">
         <div>
             <div class="justify-between">
-                                <p class="text-red-600 font-medium">This application is still in development.</p>
+                <p class="text-red-600 font-medium">This application is still in development.</p>
             </div>
         </div>
     </AppLayout>

3ae565e1aac3a8bd4ed28179364481fc9180502d
Zascia Hugo <hugozascia@gmail.com>
2024-11-15 00:02:49 +0200

    Rounded Corners

--- resources/js/Components/DialogModal.vue
+++ resources/js/Components/DialogModal.vue
@@ -1,3 +1,23 @@
+<template>
+    <div v-if="show" class="fixed inset-0 flex items-center justify-center z-50 bg-black bg-opacity-50">
+        <div :class="['bg-white rounded-lg w-96', maxWidthClass]">
+            <div class="px-6 py-4">
+                <div class="text-lg font-medium text-gray-900">
+                    <slot name="title" />
+                </div>
+
+                <div class="mt-4 text-sm text-gray-600">
+                    <slot name="content" />
+                </div>
+            </div>
+
+            <div class="flex justify-end px-6 py-4 bg-gray-100 rounded-b-lg">
+                <slot name="footer" />
+            </div>
+        </div>
+    </div>
+</template>
+
 <script setup>
 import Modal from './Modal.vue';
 
@@ -22,26 +42,3 @@ const close = () => {
     emit('close');
 };
 </script>
-
-<template>
-    <Modal
-        :show="show"
-        :max-width="maxWidth"
-        :closeable="closeable"
-        @close="close"
-    >
-        <div class="px-6 py-4">
-            <div class="text-lg font-medium text-gray-900">
-                <slot name="title" />
-            </div>
-
-            <div class="mt-4 text-xs text-gray-600">
-                <slot name="content" />
-            </div>
-        </div>
-
-        <div class="flex flex-row justify-end px-6 py-4 bg-gray-100 text-end">
-            <slot name="footer" />
-        </div>
-    </Modal>
-</template>

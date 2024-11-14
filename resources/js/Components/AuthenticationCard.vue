f1b46d628c409cbe8d4197332c15e26c4b90325c
Zascia Hugo <hugozascia@gmail.com>
2024-11-14 23:21:51 +0200

    Updated API

--- resources/js/Components/AuthenticationCard.vue
+++ resources/js/Components/AuthenticationCard.vue
@@ -1,6 +1,6 @@
 <template>
     <div class="min-h-screen flex flex-col justify-center items-center">
-        <div class="w-[350px] md:w-[400px] mt-6 px-8 py-8 bg-[#00434b] border border-[#1A565D] overflow-hidden rounded flex flex-col items-center">
+        <div class="w-[350px] md:w-[400px] mt-6 px-8 py-8 bg-[#00434b] border border-[#1A565D] overflow-hidden rounded-lg flex flex-col items-center">
             <div class="w-full flex justify-center pt-4 pb-8">
                 <slot name="logo" />
             </div>
--- resources/js/Pages/API/Index.vue
+++ resources/js/Pages/API/Index.vue
@@ -18,7 +18,7 @@ defineProps({
         </template>
 
         <div>
-            <div class="max-w-7xl mx-auto py-10 sm:px-6 lg:px-8">
+            <div>
                 <ApiTokenManager
                     :tokens="tokens"
                     :available-permissions="availablePermissions"

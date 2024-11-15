ca1185d11b1eb3505508050bcf26052bc579bb3e
Zascia Hugo <hugozascia@gmail.com>
2024-11-15 14:16:40 +0200

    Database Seeder for Cultivars

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            RolesSeeder::class,
+            // RolesSeeder::class,
+            CultivarsSeeder::class
         ]);
     }
 }

1b5e69e79e9e5d17d314863bb8381430f766c2ab
Zascia Hugo <hugozascia@gmail.com>
2024-12-23 12:07:30 +0200

    Added Teams back to the Files. Need to redo the entire program.

---
+++ resources/js/Components/TextArea.vue
@@ -0,0 +1,26 @@
+<script setup>
+import {onMounted, ref} from 'vue';
+
+defineProps({
+    modelValue: String,
+});
+
+defineEmits(['update:modelValue']);
+
+const input = ref(null);
+
+onMounted(() => {
+    if (input.value.hasAttribute('autofocus')) {
+        input.value.focus();
+    }
+});
+
+defineExpose({focus: () => input.value.focus()});
+</script>
+
+<template>
+    <textarea ref="input"
+              class="text-black text-xs placeholder:text-xs border-gray-300 focus:border-[#BCDA84] focus:ring-[#BCDA84] rounded-lg"
+              rows="5"
+              :value="modelValue" @input="$emit('update:modelValue', $event.target.value)"></textarea>
+</template>
